0d8c18f3f9a52b5ee5b7d38cd2b13f0a64091a5e
Luca Iacobini <luca@binario.it>
2019-01-28 12:40:11 +0100

    Corretta centratura pulsante

--- src/modules/Input.js
+++ src/modules/Input.js
@@ -29,7 +29,9 @@ class Input extends Component
 		return (
 			<div className={"input mh2 mv4"}>
 				<input type="text" value={value} onChange={this.inputHandler} placeholder={label} />
-				<div className={"button white bg-mid-gray"} onClick={this.clickHandler}>{"+"}</div>
+				<div className={"button white bg-mid-gray flex items-center justify-center"} onClick={this.clickHandler}>
+					<span>{"+"}</span>
+				</div>
 			</div>
 		);
 	}
